b4f82ec519ccbb4e05e125a3c507068558354314
alvaroagr <alvaro.gomez.rey@gmail.com>
2020-12-07 11:01:35 -0500

    completed signup and login. google sign-in is possible. app permissions are asked at startup.

--- app/src/main/java/com/example/infotransito/LoginActivity.java
+++ app/src/main/java/com/example/infotransito/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.infotransito;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextView signupLink;
 
     private FirebaseAuth auth;
+    private FirebaseFirestore db;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +47,72 @@ public class LoginActivity extends AppCompatActivity {
         signupLink = findViewById(R.id.signupLink);
 
         auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         loginBtn.setOnClickListener(this::login);
         signupLink.setOnClickListener(this::goToSignUp);
+        findViewById(R.id.sign_in_button).setOnClickListener(this::signIn);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void signIn(View view) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 6);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 6) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                // ...
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(
+                        task -> {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = auth.getCurrentUser();
+                                // Sign in success, update UI with the signed-in user's information
+                                HashMap<String, String> data = new HashMap<>();
+                                data.put("id", user.getUid());
+                                data.put("name", user.getDisplayName());
+                                data.put("email", user.getEmail());
+                                data.put("img", user.getPhotoUrl().toString());
+                                db.collection("users")
+                                        .document(user.getUid()).set(data).addOnCompleteListener(
+                                        dataTask -> {
+                                            if(dataTask.isSuccessful()){
+                                                goToMain();
+                                            }
+                                        }
+                                );
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                //                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                //                            Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                                //                            updateUI(null);
+                            }
+                        });
     }
 
     private void login(View v){
@@ -71,7 +150,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void goToMain() {
         Intent i = new Intent(this, MainActivity.class);
-//        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(i);
         finish();
     }
--- app/src/main/java/com/example/infotransito/MainActivity.java
+++ app/src/main/java/com/example/infotransito/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.infotransito;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,38 +39,59 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         auth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
-        if(auth.getCurrentUser() == null){
-            goToLanding();
-            return;
-        }
-
-        signoutBtn = findViewById(R.id.signoutBtn);
-        navigationView = findViewById(R.id.bottomNavigationView);
-        mapsFragment = MapsFragment.newInstance();
-        exampleFragment = ExampleFragment.newInstance();
-
-        showFragment(mapsFragment);
+//        setContentView(R.layout.activity_main);
 
 
-        signoutBtn.setOnClickListener(this::signOut);
-        navigationView.setOnNavigationItemSelectedListener(
-                (menuItem) -> {
-                    switch(menuItem.getItemId()){
-                        case R.id.maps:
-                            showFragment(mapsFragment);
-                            break;
-                        case R.id.example:
-                            showFragment(exampleFragment);
-                            break;
-                    }
-                    return true;
-                }
-        );
+
+        //
+        ActivityCompat.requestPermissions(this, new String[] {
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }, 1);
+
+//        auth = FirebaseAuth.getInstance();
+//        db = FirebaseFirestore.getInstance();
+//
+//        if(auth.getCurrentUser() == null){
+//            goToLanding();
+//            return;
+//        }
+//
+//        db.collection("users").document(auth.getUid()).get().addOnCompleteListener(
+//                documentSnapshotTask -> {
+//                    myUser = documentSnapshotTask.getResult().toObject(User.class);
+//                }
+//        );
+//
+//        signoutBtn = findViewById(R.id.signoutBtn);
+//        navigationView = findViewById(R.id.bottomNavigationView);
+//        mapsFragment = MapsFragment.newInstance();
+//        exampleFragment = ExampleFragment.newInstance();
+//
+//        showFragment(mapsFragment);
+////        showFragment(exampleFragment);
+//
+//
+//        signoutBtn.setOnClickListener(this::signOut);
+//        navigationView.setOnNavigationItemSelectedListener(
+//                (menuItem) -> {
+//                    switch(menuItem.getItemId()){
+//                        case R.id.maps:
+//                            showFragment(mapsFragment);
+//                            break;
+//                        case R.id.example:
+//                            showFragment(exampleFragment);
+//                            break;
+//                    }
+//                    return true;
+//                }
+//        );
     }
 
     private void showFragment(Fragment fragment) {
@@ -83,16 +109,18 @@ public class MainActivity extends AppCompatActivity {
 
     private void resolveCurrentUser() {
         FirebaseUser fbUser = auth.getCurrentUser();
-        db.collection("users").document(fbUser.getUid()).get().addOnCompleteListener(
-                task -> {
-                    DocumentSnapshot snapshot = task.getResult();
-                    myUser = snapshot.toObject(User.class);
-                    Toast.makeText(this,
-                            "Bienvenido " + myUser.getName(),
-                            Toast.LENGTH_LONG )
-                            .show();
-                }
-        );
+        if(fbUser != null){
+            db.collection("users").document(fbUser.getUid()).get().addOnCompleteListener(
+                    task -> {
+                        DocumentSnapshot snapshot = task.getResult();
+                        myUser = snapshot.toObject(User.class);
+                        Toast.makeText(this,
+                                "Bienvenido " + myUser.getName(),
+                                Toast.LENGTH_LONG )
+                                .show();
+                    }
+            );
+        }
 
     }
 
@@ -106,5 +134,69 @@ public class MainActivity extends AppCompatActivity {
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(i);
         finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        boolean allGrant = true;
+        if(requestCode==1){
+            for(int i=0; i< grantResults.length; i++){
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED){
+                    allGrant = false;
+                    break;
+                }
+            }
+
+            if(!allGrant){
+                Toast.makeText(this,"Si no concede todos los permisos, no puede usar " +
+                        "la aplicación.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+                return;
+            } else {
+                startup();
+            }
+
+        }
+    }
+
+    public void startup(){
+        setContentView(R.layout.activity_main);
+        if(auth.getCurrentUser() == null){
+            goToLanding();
+            return;
+        }
+
+        db.collection("users").document(auth.getUid()).get().addOnCompleteListener(
+                documentSnapshotTask -> {
+                    myUser = documentSnapshotTask.getResult().toObject(User.class);
+                }
+        );
+
+        signoutBtn = findViewById(R.id.signoutBtn);
+        navigationView = findViewById(R.id.bottomNavigationView);
+        mapsFragment = MapsFragment.newInstance();
+        exampleFragment = ExampleFragment.newInstance();
+
+        showFragment(mapsFragment);
+//        showFragment(exampleFragment);
+
+
+        signoutBtn.setOnClickListener(this::signOut);
+        navigationView.setOnNavigationItemSelectedListener(
+                (menuItem) -> {
+                    switch(menuItem.getItemId()){
+                        case R.id.maps:
+                            showFragment(mapsFragment);
+                            break;
+                        case R.id.example:
+                            showFragment(exampleFragment);
+                            break;
+                    }
+                    return true;
+                }
+        );
     }
 }
